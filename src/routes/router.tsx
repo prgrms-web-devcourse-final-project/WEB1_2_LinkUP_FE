4524da5bf0286f0e715f8e28e485d909d95a793b
sunglitter <kds0284@gmail.com>
2024-11-21 04:50:46 +0900

    Chore: 라우터 경로 임시 설정 (#2)
    - 개발 및 테스트를 위한 라우터 경로 임시 설정 추가
    - 추후 경로 확정 후 수정 예정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ErrorPage from './ErrorPage';
 import NotFoundPage from './NotFoundPage';
 import HomePage from '../components/pages/HomePage';
+import CommunityPage from '../components/pages/community/CommunityPage';
 
 const router = createBrowserRouter([
   {
@@ -11,7 +12,11 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [],
   },
-
+  {
+    path: '/community', // CommunityPage 경로
+    element: <CommunityPage />,
+    errorElement: <ErrorPage />,
+  },
   { path: '*', element: <NotFoundPage /> }, // 404 페이지
 ]);
 

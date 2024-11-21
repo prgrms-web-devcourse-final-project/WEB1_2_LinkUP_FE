8b03b16f975f3e39cbe39ab5fd86fdb9d32e0382
jiyoung <jyhan0309@gmail.com>
2024-11-21 21:23:22 +0900

    feat: signup 페이지 구성 완료

--- src/components/pages/login/SignUpPage.tsx
+++ src/components/pages/login/SignUpPage.tsx
@@ -1,7 +1,76 @@
 import React from 'react';
+import { styled } from 'styled-components';
 
 function SignUpPage() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <LeftContent>Image</LeftContent>
+      <RightContent>
+        <Title>회원가입</Title>
+        <Subtitle>email address</Subtitle>
+        <StyledInput placeholder="이메일 입력" />
+        <Subtitle>password</Subtitle>
+        <StyledInput placeholder="비밀번호 입력 (8 ~ 16자리)" />
+        <Subtitle>confirm password</Subtitle>
+        <StyledInput placeholder="비밀번호 확인" />
+        <LoginButton>Signup</LoginButton>
+      </RightContent>
+    </Wrapper>
+  );
 }
+
+const LoginButton = styled.div`
+  background-color: #000;
+  color: #fff;
+  border-radius: 5px;
+  width: calc(100% - 20px);
+  height: 35px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 35px;
+  cursor: pointer;
+`;
+
+const StyledInput = styled.input`
+  width: calc(100% - 20px);
+  height: 35px;
+  border-radius: 5px;
+  font-size: 13px;
+`;
+
+const Subtitle = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 900;
+  margin-bottom: 20px;
+`;
+const LeftContent = styled.div`
+  flex: 6;
+  background-color: lightblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const RightContent = styled.div`
+  flex: 4;
+  background-color: #fff;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  padding-left: 20px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
 
 export default SignUpPage;

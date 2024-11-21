3098bbd3b10e378ac9e62df69c18ec403ba71d69
sunglitter <kds0284@gmail.com>
2024-11-21 18:09:34 +0900

    Style: CommunityPage.tsx 스타일 수정 (#2)
    - UI 개선을 위한 여백, 폰트 크기 및 정렬 스타일 조정

--- src/components/pages/community/CommunityPage.tsx
+++ src/components/pages/community/CommunityPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import CategoryWrapper from '../../common/CategoryWrapper';
 import PostList from '../../common/PostList';
 import ScrollToTopButton from '../../common/ScrollToTopButton';
@@ -8,14 +9,32 @@ const CommunityPage = () => {
 
   return (
     <div>
-      <h1>공구 모집 및 진행</h1>
-      <CategoryWrapper
-        onCategoryChange={(categoryName) => setSelectedCategory(categoryName)}
-      />
-      <PostList selectedCategory={selectedCategory} />
+      <CommunityPageContainer>
+        <ContentWrapper>
+          <h1>공구 모집 및 진행</h1>
+          <CategoryWrapper
+            onCategoryChange={(categoryName) =>
+              setSelectedCategory(categoryName)
+            }
+          />
+          <PostList selectedCategory={selectedCategory} />
+        </ContentWrapper>
+      </CommunityPageContainer>
       <ScrollToTopButton />
     </div>
   );
 };
 
 export default CommunityPage;
+
+const CommunityPageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const ContentWrapper = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+`;

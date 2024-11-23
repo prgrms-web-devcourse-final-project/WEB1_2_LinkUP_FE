aa02f0152ae968fee671c26a92cd8e200a0c5d09
jiyoung <jyhan0309@gmail.com>
2024-11-23 15:56:44 +0900

    feat: mock data 중앙관리 형식으로 변경 (#7)

--- src/components/pages/myPage/SettingPage.tsx
+++ src/components/pages/myPage/SettingPage.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { styled } from 'styled-components';
 import GS from './GS';
 import Sidemenu from './SideMenu';
+import { SettingPageData } from './mockData';
 
 const SettingPage = () => {
-  const { addressList } = mockData;
+  const { addressList } = SettingPageData;
 
   return (
     <GS.Wrapper>
@@ -48,29 +49,6 @@ const SettingPage = () => {
       </GS.Content>
     </GS.Wrapper>
   );
-};
-
-const mockData = {
-  addressList: [
-    {
-      id: 1,
-      name: 'Robert Fox',
-      address: '4517 Washington Ave. Manchester, Kentucky 39495',
-      phone: '(209) 555-0104',
-    },
-    {
-      id: 2,
-      name: 'John Willions',
-      address: '3891 Ranchview Dr. Richardson, California 62639',
-      phone: '(270) 555-0117',
-    },
-    {
-      id: 3,
-      name: 'Alexa Johnson',
-      address: '4517 Washington Ave. Manchester, Kentucky 39495',
-      phone: '(208) 555-0112',
-    },
-  ],
 };
 
 const EditButton = styled.div`
---
+++ src/components/pages/myPage/mockData.ts
@@ -0,0 +1,22 @@
+export const SettingPageData = {
+  addressList: [
+    {
+      id: 1,
+      name: 'Robert Fox',
+      address: '4517 Washington Ave. Manchester, Kentucky 39495',
+      phone: '(209) 555-0104',
+    },
+    {
+      id: 2,
+      name: 'John Willions',
+      address: '3891 Ranchview Dr. Richardson, California 62639',
+      phone: '(270) 555-0117',
+    },
+    {
+      id: 3,
+      name: 'Alexa Johnson',
+      address: '4517 Washington Ave. Manchester, Kentucky 39495',
+      phone: '(208) 555-0112',
+    },
+  ],
+};

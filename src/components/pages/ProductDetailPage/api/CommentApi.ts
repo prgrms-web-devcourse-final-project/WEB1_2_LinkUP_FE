453fbb12aed2bd11711e848f62de1b562bb38b95
yunsung <yunsung0119@gmail.com>
2024-12-09 17:49:12 +0900

    style: css일부 수정(#1)

--- src/components/pages/ProductDetailPage/api/CommentApi.ts
+++ src/components/pages/ProductDetailPage/api/CommentApi.ts
@@ -5,16 +5,19 @@ export interface Review {
 }
 
 export const addComment = async (productId: number, payload: Review) => {
-  const URL = `/api/review/${productId}`;
-  await axiosInstance.post(URL, payload);
+  const URL = `api/review/${productId}`;
+  const response = await axiosInstance.post(URL, payload);
+  return response;
 };
 
 export const editComment = async (reviewId: number, payload: Review) => {
-  const URL = `/api/review/update/${reviewId}`;
-  await axiosInstance.put(URL, payload);
+  const URL = `api/review/update/${reviewId}`;
+  const response = await axiosInstance.put(URL, payload);
+  return response;
 };
 
 export const deleteComment = async (reviewId: number) => {
-  const URL = `/api/review/remove/${reviewId}`;
-  await axiosInstance.put(URL);
+  const URL = `api/review/remove/${reviewId}`;
+  const response = await axiosInstance.put(URL);
+  return response;
 };

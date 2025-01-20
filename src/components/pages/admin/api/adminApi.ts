f061b5bd0da0fb5f838884a630a790cfbfc3e2c2
yunsung <yunsung0119@gmail.com>
2025-01-20 17:58:47 +0900

    refactor: 관리자 api 수정

--- src/components/pages/admin/api/adminApi.ts
+++ src/components/pages/admin/api/adminApi.ts
@@ -1,65 +1,38 @@
 import axiosInstance from '../../../../api/axiosInstance';
 import { Post } from '../../../../types/postTypes';
 
-// 관리자용 승인 대기 중인 포스트 목록 조회
 export const fetchPendingPosts = async (): Promise<Post[]> => {
-  const response = await axiosInstance.get('/api/admin/post', {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-    },
-  });
-  if (response.status !== 200) {
-    throw new Error('Failed to fetch pending posts');
+  try {
+    const URL = `/api/admin/post`;
+    const response = await axiosInstance.get(URL);
+    return response.data;
+  } catch {
+    throw new Error('승인 대기 중인 포스트 목록을 가져오는 데 실패했습니다.');
   }
-  return response.data;
 };
 
-// 관리자용 포스트 승인 처리
 export const approvePost = async (
-  communityPostId: number,
+  postId: number,
   title: string
 ): Promise<Post> => {
   try {
-    const response = await axiosInstance.patch(
-      `/api/admin/post/approve/${communityPostId}`,
-      { title },
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-        },
-      }
-    );
-    if (response.status !== 200) {
-      throw new Error('Failed to approve post');
-    }
+    const URL = `/api/admin/post/approve/${postId}`;
+    const response = await axiosInstance.patch(URL, title);
     return response.data;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    throw new Error(`Error approving post with ID ${communityPostId}`);
+  } catch {
+    throw new Error(`ID ${postId}의 포스트 승인을 처리하는 데 실패했습니다.`);
   }
 };
 
-// 관리자용 포스트 거절 처리
 export const rejectPost = async (
-  communityPostId: number,
+  postId: number,
   title: string
 ): Promise<Post> => {
   try {
-    const response = await axiosInstance.patch(
-      `/api/admin/post/reject/${communityPostId}`,
-      { title },
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-        },
-      }
-    );
-    if (response.status !== 200) {
-      throw new Error('Failed to reject post');
-    }
+    const URL = `/api/admin/post/reject/${postId}`;
+    const response = await axiosInstance.patch(URL, title);
     return response.data;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    throw new Error(`Error rejecting post with ID ${communityPostId}`);
+  } catch {
+    throw new Error(`ID ${postId}의 포스트 거절을 처리하는 데 실패했습니다.`);
   }
 };
